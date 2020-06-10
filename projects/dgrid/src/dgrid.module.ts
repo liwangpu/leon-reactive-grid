e4309f054a1df99575eac6101210480029dd9485
Leon Pu <puliwang@cxist.com>
2020-06-10 09:43:53 +0800

    1

--- projects/dgrid/src/dgrid.module.ts
+++ projects/dgrid/src/dgrid.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GridComponent, GridHeaderComponent, GridContentComponent, GridFooterComponent } from './components';
+import { GridStoreModule } from './state-store';
 
 
 @NgModule({
     declarations: [GridComponent, GridHeaderComponent, GridContentComponent, GridFooterComponent],
     imports: [
-        CommonModule
+        CommonModule,
+        GridStoreModule
     ],
     exports: [
         GridComponent
---
+++ projects/dgrid/src/state-store/states/i-grid-state.ts
@@ -0,0 +1,23 @@
+import { IFilterView } from '../../models/i-filter-view';
+
+export interface IGridState {
+    grid: { [key: string]: IGridData };
+}
+
+export interface IGridData {
+    activeViewId?: string;
+    keyword?: string;
+    pagination?: IGridPagination;
+    sorting?: IGridSorting;
+    views: { [key: string]: Array<IFilterView> };
+}
+
+export interface IGridPagination {
+    page?: number;
+    limit?: number
+}
+
+export interface IGridSorting {
+    field?: string;
+    direction?: string
+}
