286ebda891dec2538f602a8507771d0ef84b1ac6
Leon Pu <puliwang@cxist.com>
2020-06-12 11:18:05 +0800

    2

--- projects/dgrid/src/grid-store/grid-store.module.ts
+++ projects/dgrid/src/grid-store/grid-store.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 
 @NgModule({
-  declarations: [],
   imports: [
-    CommonModule
+
   ]
 })
 export class GridStoreModule { }
---
+++ projects/dgrid/src/grid-store/state.ts
@@ -0,0 +1,14 @@
+import { EntityState } from '@ngrx/entity';
+
+export interface GridState extends EntityState<IGrid> {
+
+}
+
+
+export interface IGrid {
+
+}
+
+export interface IGrid {
+
+}
