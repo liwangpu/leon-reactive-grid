e97820cd59caf9d31395b22aeb116283fffa2a13
Leon Pu <puliwang@cxist.com>
2020-06-12 14:36:24 +0800

    2

--- projects/dgrid/src/grid-store/action.ts
+++ projects/dgrid/src/grid-store/action.ts
@@ -1,9 +1,7 @@
 import { createAction, props } from '@ngrx/store';
 import * as fromState from './state';
-import { Update } from '@ngrx/entity';
 
-export const addGrid = createAction('[DGrid] Add Grid', props<{ grid: fromState.IGridData }>());
-export const updateGrid = createAction('[DGrid] Update Grid', props<{ update: Update<fromState.IGridData> }>());
 
 export const initGrid = createAction('[DGrid] init grid', props<{ id: string }>());
-export const changePagination = createAction('[DGrid] change pagination', props<{ id: string; page?: number; limit?: number }>());
+export const setPagination = createAction('[DGrid] change pagination', props<{ id: string; page?: number; limit?: number }>());
+// export const 
