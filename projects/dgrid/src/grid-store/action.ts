1b3e2ac09f084674191aeabad24d96bf1457f8cc
Leon Pu <puliwang@cxist.com>
2020-06-12 15:16:12 +0800

    2

--- projects/dgrid/src/grid-store/action.ts
+++ projects/dgrid/src/grid-store/action.ts
@@ -3,5 +3,5 @@ import * as fromState from './state';
 import * as fromModel from '../models';
 
 export const initGrid = createAction('[DGrid] init grid', props<{ id: string }>());
-export const initViews = createAction('[DGrid] change pagination', props<{ id: string; views: Array<fromModel.IFilterView> }>());
+export const initViews = createAction('[DGrid] init views', props<{ id: string; views: Array<fromModel.IFilterView> }>());
 export const changePagination = createAction('[DGrid] change pagination', props<{ id: string; page?: number; limit?: number }>());
